939ef20c7c6820471b3c5efa9b6094a381f91bfe
WorldWideDev <dev206@gmail.com>
2018-07-09 17:44:30 -0700

    lecture code w1s1

---
+++ 1_Week/1_Session/MyNewThingYay/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyNewThingYay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ENTRY POINT
+
+            // Variables (primitaves)
+            int number = 5;
+            long bigNumber = 200;
+            float speed = 5.5f;
+            double aDouble = 50.4;
+            string name = "Devon";
+            bool isCool = true;
+
+            int someOtherNumber;
+            someOtherNumber = 10;
+
+            // CASTING
+            double maxSpeed = speed + aDouble;
+            float fMaxSpeed = (float)maxSpeed;
+
+            // WHAT IS THIS?
+            int iDouble = (int)aDouble;
+
+            // Variables (complex)
+            // array?
+            int[] numbers = new int[]
+            {
+                10, 12, 21, -23, 10
+            };
+
+            Console.WriteLine(iDouble);
+            Console.WriteLine(fMaxSpeed);
+            Console.WriteLine(numbers[0]);
+            // [10,12,21,-23,10]
+        
+        }
+    }
+}
